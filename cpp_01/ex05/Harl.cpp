88ce6b767110fca7e6318f0e67c4081890c4715e
wet6123 <gentlmanjun@naver.com>
2024-04-22 21:35:01 +0900

    impl: change message

--- cpp_01/ex05/Harl.cpp
+++ cpp_01/ex05/Harl.cpp
@@ -2,22 +2,22 @@
 
 void Harl::debug(void)
 {
-	std::cout << "DEBUG\n";
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
 }
 
 void Harl::info(void)
 {
-	std::cout << "INFO\n";
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
 }
 
 void Harl::warning(void)
 {
-	std::cout << "WARNING\n";
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\n";
 }
 
 void Harl::error(void)
 {
-	std::cout << "ERROR\n";
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
 }
 
 void Harl::complain(std::string level)
