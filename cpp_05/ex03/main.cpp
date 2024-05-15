12804e6bb1ea48564b4bb448c15c6d246fa04c26
JunSeong <gentlmanjun@naver.com>
2024-05-15 12:14:12 +0900

    feat: impl ex03 main

--- cpp_05/ex03/main.cpp
+++ cpp_05/ex03/main.cpp
@@ -8,36 +8,13 @@ int main()
 {
     try
 	{
-		Bureaucrat polarbear("polarbear", 1);
-		Bureaucrat seongwol("seongwol", 50);
-		Bureaucrat junsbae("junsbae", 140);
-
-		// Required grades: sign 145, exec 137
-		ShrubberyCreationForm shrubberyForm("test1");
-		// Required grades: sign 72, exec 45
-		RobotomyRequestForm robotomyForm("test2");
-		// Required grades: sign 25, exec 5
-		PresidentialPardonForm presidentialForm("test3");
+		Bureaucrat junsbae("junsbae", 1);
+		Intern intern;
+		AForm *shrubberyForm = intern.makeForm("shrubbery creation", "home");
 
 		// ShrubberyCreationForm
 		junsbae.signForm(shrubberyForm);
-		seongwol.signForm(shrubberyForm);
 		junsbae.executeForm(shrubberyForm);
-		seongwol.executeForm(shrubberyForm);
-		std::cout << "\n";
-
-		// RobotomyRequestForm
-		junsbae.signForm(robotomyForm);
-		seongwol.signForm(robotomyForm);
-		junsbae.executeForm(robotomyForm);
-		polarbear.executeForm(robotomyForm);
-		std::cout << "\n";
-
-		// PresidentialPardonForm
-		junsbae.signForm(presidentialForm);
-		// polarbear.signForm(presidentialForm);
-		junsbae.executeForm(presidentialForm);
-		polarbear.executeForm(presidentialForm);
 	}
 	catch(const std::exception& e)
 	{
