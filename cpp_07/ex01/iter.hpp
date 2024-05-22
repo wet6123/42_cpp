f084be896e2990226165e3113c532a7d9eb264ca
JunSeong, Bae <gentlmanjun@naver.com>
2024-05-22 06:35:41 +0000

    feat: impl cpp07 ex01

---
+++ cpp_07/ex01/iter.hpp
@@ -0,0 +1,13 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <iostream>
+
+template<typename T>
+void iter(T* address, size_t length, void (*f)(T const &value))
+{
+    for (size_t i = 0; i < length; i++)
+        f(address[i]);
+}
+
+#endif
---
+++ cpp_07/ex01/main.cpp
@@ -0,0 +1,21 @@
+#include "iter.hpp"
+#include <iostream>
+
+void print(const int &value)
+{
+    std::cout << value << std::endl;
+}
+
+void print(const std::string &value)
+{
+    std::cout << value << std::endl;
+}
+
+int main()
+{
+    int int_arr[] = {1, 2, 3, 4, 5};
+    std::string str_arr[] = {"Hello", "42", "World", "!!"};
+
+    ::iter(int_arr, (sizeof(int_arr)/sizeof(int)), print);
+    ::iter(str_arr, (sizeof(str_arr)/sizeof(std::string)), print);
+}
