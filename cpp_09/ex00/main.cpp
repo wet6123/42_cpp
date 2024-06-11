f9b7abeac189f7ec6585fcc94c786dca3d24198c
wet6123 <gentlmanjun@naver.com>
2024-06-11 18:22:42 +0900

    Comment: remove annotation

--- cpp_09/ex00/main.cpp
+++ cpp_09/ex00/main.cpp
@@ -12,14 +12,6 @@ int main(int argc, char **argv)
 		std::cerr << "Error: Invalid arguments" << std::endl;
 		return (1);
 	}
-	// data.csv 파일을 읽음
-	// data.csv 파일을 읽어서 data에 저장
-	// 입력된 파일을 읽음
-	// 날짜 => 개수 = 가격 형식으로 출력
-	//  ㄴ 에러처리
-	//  ㄴ 개수 범위 0 ~ 1000
-	//  ㄴ 날짜 범위
-	// 파일을 닫음
 
 	BitcoinExchange exchange;
 	exchange.printExchange(argv[1]);
