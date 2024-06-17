847131011adfdb1f531fd39cfa71c7d71449773a
JunSeong <gentlmanjun@naver.com>
2024-06-17 16:12:56 +0900

    feat: impl sort checker

---
+++ cpp_09/sortChecker/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+int main (int argc, char **argv)
+{
+  std::vector<int> origin;
+  std::vector<int> sorted;
+
+  for (int i = 1; i < argc; i++)
+  {
+    for (int j = 0; argv[i][j]; j++)
+    {
+      if (std::isdigit(argv[i][j]) == 0)
+      {
+        std::cerr << "Invalid argument" << std::endl;
+        return (1);
+      }
+    }
+    int num = std::strtod(argv[i], NULL);
+    origin.push_back(num);
+  }
+
+  sorted = origin;
+  sort(sorted.begin(), sorted.end());
+
+  for(std::vector<int>::iterator it = sorted.begin(); it != sorted.end(); it++)
+  {
+    if(*it != origin[it - sorted.begin()])
+    {
+      std::cout << "KO" << std::endl;
+      return (0);
+    }
+  }
+
+  std::cout << "OK" << std::endl;
+
+  return (0);
+}
